0a6d094e2da3a85fcdec96758bf7a058a4841cdc
Gayle Ocampo <gocampo@butler.edu>
2015-11-09 00:12:11 -0500

    Added view controllers for filled/unfilled prescriptions

---
+++ emr-prototype-v1/FilledPrescriptionTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FilledPrescriptionTableViewController.swift
+//  emr-prototype-v1
+//
+//  Created by Gayle Ocampo on 11/9/15.
+//  Copyright © 2015 Gayle Ocampo. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+import Bolts
+
+class FilledPrescriptionTableViewController: PFQueryTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController!.navigationBar.translucent = false;
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
