abe8fbf24d351482de266ceba0cf55484dd88a68
Gayle Ocampo <gocampo@butler.edu>
2015-11-08 22:16:15 -0500

    Add View Controller Files

---
+++ emr-prototype-v1/PatientDashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PatientDashViewController.swift
+//  emr-prototype-v1
+//
+//  Created by Gayle Ocampo on 11/8/15.
+//  Copyright © 2015 Gayle Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class PatientDashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ emr-prototype-v1/PatientDirectoryTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PatientDirectoryTableViewController.swift
+//  emr-prototype-v1
+//
+//  Created by Gayle Ocampo on 11/8/15.
+//  Copyright © 2015 Gayle Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class PatientDirectoryTableViewController: PFQueryTableViewController {
+
+}
