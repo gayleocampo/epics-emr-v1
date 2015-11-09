11ecc8bd00b69280fb8893cc445cf73569b05c02
Gayle Ocampo <gocampo@butler.edu>
2015-11-08 23:39:22 -0500

    Added code to handle treated/untreated patient lists

--- emr-prototype-v1/TreatedPatientTableViewController.swift
+++ emr-prototype-v1/TreatedPatientTableViewController.swift
@@ -13,26 +13,75 @@ import Bolts
 
 class TreatedPatientTableViewController: PFQueryTableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //initialize the PFQueryTableView
+    override init(style: UITableViewStyle, className: String!) {
+        super.init(style: style, className: className)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)!
+        
+        //configure the PFQueryTableView
+        self.parseClassName = "Patients"
+        self.textKey = "patientFullName"
+        self.pullToRefreshEnabled = true
+        self.paginationEnabled = false
     }
-    */
+    
+    //define the query that will provide the data for the table view
+    override func queryForTable() -> PFQuery {
+        let query = PFQuery(className: "Patients")
+        query.orderByAscending("patientFullName")
+        query.whereKey("patientTreated", equalTo: true)
+        
+        return query
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        //data reloading from parse
+        self.loadObjects()
+        
+        self.navigationController!.navigationBar.translucent = false;
+        
+        //activate and display the edit button
+        //self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
+        
+        //create a cell
+        var cell = tableView.dequeueReusableCellWithIdentifier("patientCell") as! PFTableViewCell!
+        
+        //ensure the cell exists -- Parse recommends that
+        if cell == nil {
+            cell = PFTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "patientCell")
+        }
+        
+        //extract values from the PFObject to dispaly in the table view
+        if let patient = object!["patientFullName"] as? String {
+            cell?.textLabel?.text = patient
+            
+        }
+        
+        return cell
+    }
+    
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == .Delete {
+            
+            //get current object from the parse table
+            let currentPatient: PFObject = objects![indexPath.row] as! PFObject
+            
+            //delete object in background without blockng the app resuming
+            currentPatient.deleteInBackground()
+            
+            //reload from server
+            self.loadObjects();
+            
+        }
+    }
 
 }
